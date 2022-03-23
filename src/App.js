bbf65d1ae15a2bbd09152c67ac68497c10d1a4b5
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-23 08:40:06 +0600

    cart

--- src/App.js
+++ src/App.js
@@ -16,4 +16,14 @@ function App() {
 
 export default App;
 
+//netlify
+//=======================================================
 //in comment: npm run build , will create a file which have to deliver to netlify.
+
+//github
+//=======================================================
+//cloning from the github, the react app needs to install dependency. by CMD, type: npm install
+
+//to see in which repository the project belongs to, type: git remote -v 
+
+//to change the to new repository, type: git remote set-url origin theUrl
