0a9fbf3c1f0b46e5d47f23da27e42559a4a6ab8a
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-31 19:07:36 +0600

    adding delete button

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import useProducts from '../../hooks/useProducts';
-import "./Orders.css"
+import React from "react";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css";
 
 const Orders = () => {
-    const [products, setProducts] = useProducts()
-    return (
-        <div>
-            <h2>this is order page!!: {products.length}</h2>
-        </div>
-    );
-};
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
 
+    const handleRemoveProduct = product =>{
+        console.log(product);
+    }
+
+  return (
+    <div className="shop-container">
+      <div className="review-items-container">
+          {
+              cart.map(product => <ReviewItem 
+                product={product} 
+                key={product.id} 
+                handleRemoveProduct={handleRemoveProduct}></ReviewItem>)
+          }
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
+};
 export default Orders;
